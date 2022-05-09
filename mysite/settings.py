b40c450f9291c0a558a3596d6aa53595c8845695
ivoshet <ivan.v.shet@gmail.com>
2022-05-09 15:16:57 +0500

    created blog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+# add import os for os.path.join...
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
